fdd9f6ec6036b6f9f6e049e35b38c07f60a11782
BaekTree <etknbe@gmail.com>
2019-06-07 13:40:02 +0900

    command pattern in while

--- Command/TorchRingCommand2.java
+++ Command/TorchRingCommand2.java
@@ -1,3 +1,33 @@
+/**
+ * Torch와 Ring이라는 객체들이 있다. 
+ * 각각의 버튼은 이 둘을 association한다.
+ * 횃불 버튼이 눌리면 횃불을 켠다.
+ * 종 버튼이 눌리면 종을 울린다.
+ * 
+ * 버튼을 누르면 작동한다는 비슷한 기능이 있으니 묶을 수 있다. 
+ * 일일히 두개 다 따로 따로 만들지 않아도 된다. 
+ * 
+ *  전략:
+ *      버튼을 누르면 작동을 하는 방법은 유지한다. 
+ *      횃불과 종 대신에 command class을 만든다. 
+ * 
+ * **/
+
+
+//Reciever 1
+class Torch {
+    public void turnOn() {
+        System.out.println("turn on the Torch");
+    }
+}
+
+// Reciever 2
+class Ring {
+    public void turnOn() {
+        System.out.println("Ring on");
+    }
+}
+
 //Invoker
 class Button{
     Command command;
