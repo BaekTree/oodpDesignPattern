2b589536026d3e88d412a3db36f27f097978d676
BaekTree <etknbe@gmail.com>
2019-06-08 21:12:08 +0900

    good code 22 test

--- WhatIsGoodCode/goodCode3.java
+++ WhatIsGoodCode/goodCode3.java
@@ -1,12 +1,16 @@
 /**
  * goodcode3
  * 
- * 문제점: 
+ * goodcode2의 문제점: 
  * Client에서 InterfaceOne에 dependency하고 있다.
  * Interface에 해당하는 ClassOne, ClassTwo 등의 instance을 polymorphysm을 통해서 reference해주어야 한다.
  * 그런데 reference하기 위해서 직접적으로 new ClassOne, new ClassTwo등을 사용했었다.
  * ClassOne, ClassTwo는 concerete class으로써, 이 class들에 직접적으로 dependency 하게 되면 concrete 클래스에 변경이 생길 때
- * (새로운 attributes을 추가해서 constructor에 변화가 생긴다던지, 새로운 )
+ * (새로운 attributes을 추가해서 constructor에 변화가 생긴다던지, method에 parameter가 추가되어야 한다든지...)
+ * Client에서도 똑같이 바꿔줘야 했다.
+ * 
+ * 
+ * 
  * 
  * **/
 interface InterfaceOne {
@@ -32,16 +36,24 @@ class Client{
     }
 
     public void setItfOne(){
-        this.itf = new ClassOne();  //실제 ClassOne에 dependency 되어있다.
-                                    // ClassOne에 수정이 발생하면 이 부분도 수정해주어야 한다.
+        itf = ClassFactory.makeOne();
     }
 
     public void setItfTwo(){
-        this.itf = new ClassTwo();// 실제 ClassTwo에 dependency 되어있다.
-                                  // ClassTwo에 수정이 발생하면 이 부분도 수정해주어야 한다.
+        itf = ClassFactory.makeTwo();
+    }
+}
+
+
+class ClassFactory{
+    public static InterfaceOne makeOne(){
+        return new ClassOne();
     }
 
-
+    public static InterfaceOne makeTwo() {
+        return new ClassTwo();
+    }
+    
 }
 
 class Driver{
