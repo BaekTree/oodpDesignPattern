192c870d66e4fd5f37885040d496213cbc36d2c0
BaekTree <etknbe@gmail.com>
2019-06-08 23:16:58 +0900

    single duck

--- Strategy/strategy.java
+++ Strategy/strategy.java
@@ -1,46 +1,105 @@
-class Duck{
+import java.util.Scanner;
+
+class Driver{
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         
-        Fly ff;
-        Move mv;
-    
-        ff = new Float();
-        ff.fly();
-        mv = new Sprint();
-        mv.move();
+        while(true){
+            System.out.println("choice fly mode");
+            System.out.println("\tfloat: 1\n\twing: 2\n\tquit: 0");
+            int choice = input.nextInt();
+            if(choice == 0)
+                break;
+            
+            Duck duck = new Duck();
+            
+            if(choice == 1){
+                duck.setFly("F");
+            }
+            else if(choice == 2){
+                duck.setFly("W");
+            }
+            else{
+                System.out.println("Wrong number!");
+                continue;
+            }
+
+            duck.fly();
+
+
+        }
+        System.out.println("BaBye~~!");
     }
-    
 
 }
 
-interface Move{
-    void move();
+enum Fly{
+    FLOAT(1),
+    WING(2);
+
+    private int val;
+    private Fly(int i){
+        this.val = i;
+    }
+    public int getValue(){
+        return val;
+    }
 }
 
-interface Fly{
-    void fly();
-}
+class Duck{
+    InterfaceFly ff;
+    InterfaceMove mv;
 
-class Float implements Fly{
     public void fly(){
+        ff.execute();
+    }
+
+    public void move(){
+        mv.execute();
+    }
+
+    public void setFly(String str){
+        try{
+            if(str.compareTo("F") == 0)
+                this.ff = new Float();
+            else if(str.compareTo("W") == 0)
+                this.ff = new Wing();
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
+
+interface InterfaceFly{
+    void execute();
+}
+
+class Float implements InterfaceFly{
+    public void execute(){
         System.out.println("Float");
     }
 }
 
-class Wing implements Fly{
-    public void fly(){
+class Wing implements InterfaceFly{
+    public void execute(){
         System.out.println("Wing");
     }
 }
 
-class Walk implements Move{
-    public void move(){
+
+interface InterfaceMove{
+    void execute();
+}
+
+class Walk implements InterfaceMove{
+    public void execute(){
         System.out.println("walk");
     } 
 }
 
-class Sprint implements Move{
-    public void move(){
+class Sprint implements InterfaceMove{
+    public void execute(){
         System.out.println("sprint");
     }
 }
